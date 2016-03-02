61f3cb904532a6f36f77537918352582594a1b66
leocavalcante <lc@leocavalcante.com>
2016-03-02 18:19:40 -0300

    Add a minimal Redux sample

--- entry.js
+++ entry.js
@@ -1,6 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { PropTypes } from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
 
-const App = () => <p>It works!</p>
+const initialState = {val: 10}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INC':
+      return Object.assign({}, state, {val: state.val + 1})
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer, initialState)
+
+const App = ({ val, inc }) => (
+  <p>
+    It works! {val}<br/>
+    <button role="button" onClick={inc}>
+      Click to inc
+    </button>
+  </p>
+)
+
+App.propTypes = {
+  val: PropTypes.number,
+  inc: PropTypes.func
+}
+
+const Container = connect(
+  state => ({
+    val: state.val
+  }),
+  dispatch => ({
+    inc: () => dispatch({type: 'INC'})
+  })
+)(App)
+
+render(
+  <Provider store={store}>
+    <Container />
+  </Provider>,
+  document.getElementById('root')
+)
